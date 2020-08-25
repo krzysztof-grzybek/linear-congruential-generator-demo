a4f8baec05107fe150da177a0f7617ae62fbc629
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2020-08-25 21:03:25 +0200

    upgrade ui

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 let nextRandom;
 const ctx = document.getElementById('canvas').getContext('2d');
-const uIntArr = new Uint32Array();
 
-// TODO: support different output bits
 function init() {
   document.forms.main.addEventListener('submit', e => {
     e.preventDefault();
     const form = e.target;
 
     if (!isFormValid(form)) {
-      alert('Please provide 32bit unsigned int values');
+      alert('Please provide numbers between 0 and 2^32');
       return;
     }
     const values = [
@@ -24,14 +22,13 @@ function init() {
   });
 
   document.getElementById('draw-btn').addEventListener('click', () => {
-    console.log(nextRandom());
     draw();
   })
 }
 
 function isFormValid(mainForm) {
   const isValid = [mainForm.a.value, mainForm.c.value, mainForm.m.value, mainForm.seed.value].every(val => {
-    return isNumeric(val.trim()) && Number(val) < Math.pow(2, 32);
+    return isNumeric(val.trim()) && Number(val) <= Math.pow(2, 32);
   });
 
   return isValid;
@@ -47,33 +44,36 @@ function updateCurrentSetup(a, c, m, seed) {
   row.cells[1].innerHTML = c;
   row.cells[2].innerHTML = m;
   row.cells[3].innerHTML = seed;
+  document.getElementById('draw-btn').removeAttribute('disabled');
 }
 
 function draw() {
   let imgData = ctx.createImageData(1024, 1024);
-  let random = nextRandom();
-  let randomBinaryString = random.toString(2).padStart(32, '0');
+  let random;
+  let randomBinaryString = '';
 
-  for (let i = 0; i < imgData.data.length; i += 4) {
-    if (randomBinaryString[i % 32] === '0') {
-      imgData.data[i+0] = 255;
-      imgData.data[i+1] = 255;
-      imgData.data[i+2] = 255;
-      imgData.data[i+3] = 255;
-    } else {
-      imgData.data[i+0] = 0;
-      imgData.data[i+1] = 0;
-      imgData.data[i+2] = 0;
-      imgData.data[i+3] = 0;
-    }
+  for (let i = 0; i < 1024 * 1024; i += 1) {
 
-    if (typeof randomBinaryString[i % 32 + 1] === 'undefined') {
+    if (i % 32 === 0) {
       random = nextRandom();
       randomBinaryString = random.toString(2).padStart(32, '0');
     }
+
+    const j = i*4;
+    if (randomBinaryString[i % 32] === '0') {
+      imgData.data[j+0] = 255;
+      imgData.data[j+1] = 255;
+      imgData.data[j+2] = 255;
+      imgData.data[j+3] = 255;
+    } else {
+      imgData.data[j+0] = 0;
+      imgData.data[j+1] = 0;
+      imgData.data[j+2] = 0;
+      imgData.data[j+3] = 255;
+    }
   }
 
-  ctx.putImageData(imgData, 10, 10);
+  ctx.putImageData(imgData, 0, 0);
 }
 
 function buildGenerator(a, c, m, seed) {
